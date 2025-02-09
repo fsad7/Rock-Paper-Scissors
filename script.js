8a2a7720112f07ca5f7d435a189086d0c67dcc58
fsad <fsad07@gmail.com>
2025-02-09 09:14:12 -0500

    Added JS

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
+// UI
+document.body.style.backgroundColor = "#FFF2F2";
+let header = document.createElement("div");
+// header:
+
+header.textContent = "Rock Paper Scissors Game";
+header.style.color = "#2D336B";
+header.style.display = "flex";
+header.style.justifyContent = "center";
+header.style.marginBottom = "20px";
+document.body.appendChild(header);
+
+// container:
+let container = document.createElement("div");
+let scores = document.createElement("div");
+let btnRock = document.createElement("button")
+let btnPaper = document.createElement("button");
+let btnScissors = document.createElement("button");
+let winLoseDisplay = document.createElement("div");
+let displayResult = document.createElement("div");
+
+// button style:
+btnRock.textContent = "Rock";
+btnPaper.textContent = "Paper";
+btnScissors.textContent = "Scissors";
+btnRock.style.borderRadius = "10px";
+btnPaper.style.borderRadius = "10px";
+btnScissors.style.borderRadius = "10px";
+btnRock.style.borderColor = "#7886C7"
+btnPaper.style.borderColor = "#7886C7"
+btnScissors.style.borderColor = "#7886C7"
+
+
+// scores style:
+scores.textContent = "0 : 0";
+scores.style.border = "1px solid #A9B5DF";
+scores.style.padding = "4px";
+
+winLoseDisplay.textContent = "Choose a option";
+
+
+
+
+
+
+// container style:
+container.style.display = "flex";
+container.style.justifyContent = "center";
+container.style.gap = "8px";
+
+displayResult.style.display = "flex";
+displayResult.style.flexDirection = "column";
+displayResult.style.alignItems = "center"
+displayResult.style.gap = "8px";
+displayResult.style.margin = "20px";
+
+container.appendChild(btnRock);
+container.appendChild(btnPaper);
+container.appendChild(btnScissors);
+
+displayResult.appendChild(scores);
+displayResult.appendChild(winLoseDisplay);
+document.body.appendChild(displayResult);
+document.body.appendChild(container);
+
+
+
+let count = 0;
 
 
 // Global variables for human and computer score
@@ -17,67 +85,82 @@ function getComputerChoice(){
 }
 
 
-function getHumanChoice(){
-    let choice = prompt("Enter rock, paper or scissors");
-    console.log("Your Choice: " + choice);
-    return choice.toUpperCase();
-}
-
 
 
 function playGame(){
-    function playRound(humanChoice, computerChoice){
+
+    function playRound(humanChoice){
+        let msg = ""
+        let computerChoice = getComputerChoice();
+        count++;
         if (humanChoice == computerChoice){
-            console.log("Tie!");
+            msg = "Tie!"
             
         }else if(humanChoice == "ROCK"){ // all choices for rock
             if (computerChoice == "PAPER"){
                 // case where human chooses rock and computer chooses paper
-                console.log("You Lose! Paper beats rock.");
+                msg = "You Lose! Paper beats rock.";
                 computerScore++;
             }else{ // computer choice must be scissors then;
-                console.log("You win! Rock beats scissors.");
+                msg = "You win! Rock beats scissors.";
                 humanScore++;
             }
         }else if (humanChoice == "PAPER"){ // all choices for paper
             if (computerChoice == "ROCK"){
-                console.log("You win!.Paper beats rock" );
+                msg = "You win!.Paper beats rock";
+                
                 humanScore++;
             }else {
-                console.log("You lose. Scissors beats paper.");
+                msg = "You lose. Scissors beats paper.";
                 computerScore++;
             }
         // all choices for scissors
         }else if (humanChoice == "SCISSORS"){
             if (computerChoice == "PAPER") {
-                console.log("You win! Scissors beats paper.");
+                msg = "You win! Scissors beats paper.";
                 humanScore++;
 
             } else {
-                console.log("You lose. Rock beats scissors");
+                msg = "You lose. Rock beats scissors";
                 computerScore++;
             }
         } else{
-            console.log("Invalid entry. Please choose rock, paper, or scissors.");
+            msg = "Invalid entry. Please choose rock, paper, or scissors.";
         }
-    }
+        winLoseDisplay.textContent = msg;
+        console.log(count);
+        if (count == 5){
+            let resultMsg = "";
+            if (humanScore > computerScore){
+                resultMsg = "Congratulations! You won the game!";
+            } else if (humanScore < computerScore){
+                resultMsg = "Sorry! You lost the game.";
+            } else {
+                resultMsg = "It's a tie!";
+            }
+            winLoseDisplay.textContent = resultMsg;
+            humanScore = 0;
+            computerScore = 0;
+            count = 0;
+        }
+        scores.textContent = + humanScore + " : " + computerScore;
 
-    let count = 0;
-    while (count < 5){
-        let computerChoice = getComputerChoice(); 
-        let humanChoice = getHumanChoice();
-        playRound(humanChoice, computerChoice);
-        count++;
+    } 
+    btnRock.addEventListener("click", () => {
+        playRound("ROCK");
+    });
 
-    }
-    console.log(`Final Score: You ${humanScore} - ${computerScore} Computer`);
-    if (humanScore > computerScore){
-        console.log("Congratulations! You won the game!");
-    } else if (humanScore < computerScore){
-        console.log("Sorry! You lost the game.");
-    } else {
-        console.log("It's a tie!");
-    }
+    btnPaper.addEventListener("click", () => {
+        playRound("PAPER");
+    });
+
+    btnScissors.addEventListener("click", () => {
+        playRound("SCISSORS");
+    });
+    
+
 }
 
+
 playGame();
+
